8b3969b2ad873f52f5f55facdc2db4e66fcacfcb
juicydrey <my.sabaka@gmail.com>
2017-11-14 14:42:18 -0500

    initial commit

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,40 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('should show rentals as the home page.', (assert) => {
+  visit('/');
+  andThen(() => {
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+});
+
+test('should link to information about the company.', (assert) => {
+  visit('/');
+  click('a:contains("About")');
+  andThen(() => {
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+});
+
+test('should link to contact information.', (assert) => {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(() => {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+});
+
+test('should list available rentals.', (assert) => {
+  visit('/');
+  andThen(() => {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
+
+test('should filter the list of rentals by city.', (assert) => {
+});
+
+test('should show details for a selected rental.', (assert) => {
+});
